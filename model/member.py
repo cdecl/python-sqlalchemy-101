c6e8d36561063b08943cdbf83dd9cc580cc64768
BK KIM <>
2020-11-24 14:43:18 +0900

    fix: mysql model

--- model/member.py
+++ model/member.py
@@ -9,12 +9,12 @@ metadata = Base.metadata
 class Customer(Base):
     __tablename__ = 'Customer'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255))
-    address = Column(String(255))
-    mobileno = Column(String(100))
-    email = Column(String(255))
-    bod = Column(String(255))
+    ID = Column(Integer, primary_key=True)
+    Name = Column(String(255))
+    Address = Column(String(255))
+    MobileNo = Column(String(100))
+    Email = Column(String(255))
+    BOD = Column(String(255))
 
 
 class CustomerTo(Base):
